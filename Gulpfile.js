5384298f8f0cd0ecc31319ef36cde582dcb1add3
Jakub Butkiewicz <jakubbutkiewicz@MacBook-Air-Jakub.local>
2018-08-23 12:01:58 +0200

    initial commit

---
+++ Gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp');  
+const sass = require('gulp-sass');  
+const browserSync = require('browser-sync');
+
+
+gulp.task('sass', function () {  
+    gulp.src('sass/app.scss')
+        .pipe(sass({includePaths: ['scss']}))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('browser-sync', function() {  
+    browserSync.init(["*.html","css/*.css", "js/*.js"], {
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('default', ['sass', 'browser-sync'], function () {  
+    gulp.watch("sass/**", ['sass']);
+});
---
+++ js/app.js
@@ -0,0 +1,32 @@
+new fullpage("#fullpage", {
+  //options here
+  menu: ".main-menu",
+  lockAnchors: false,
+  anchors: ["home", "projects", "news", "contact"],
+  navigation: true,
+  autoScrolling: true,
+  css3: true,
+  scrollingSpeed: 700,
+  autoScrolling: true,
+  fitToSection: true,
+  scrollBar: false,
+  easing: "easeInOutCubic",
+  easingcss3: "ease",
+  loopHorizontal: true,
+  scrollHorizontally: true,
+  responsiveSlides: true,
+  interlockedSlides: false,
+  dragAndMove: true,
+  offsetSections: true,
+  resetSliders: true,
+  fadingEffect: true,
+  touchSensitivity: 15,
+  controlArrows: true,
+  normalScrollElementTouchThreshold: 5,
+  fixedElements: ".contact , .logo , .main-nav",
+  keyboardScrolling: true,
+  animateAnchor: true,
+  parallax: true
+});
+
+//methods
